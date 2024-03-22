3eca43de41b4e38053ce03ce20722599fc5fc766
krittanai-chat <161231446+krittanai-chat@users.noreply.github.com>
2024-03-22 23:50:42 +0700

    Add files via upload

---
+++ Ass6-ML-66130701701.py
@@ -0,0 +1,27 @@
+
+import streamlit as st
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Load model_bay
+model_bay = pickle.load(open('C:/Users/User/Downloads/naive_bayes-66130701701.sav', 'rb'))
+
+# Load Vectorizer
+vectorizer = pickle.load(open('C:/Users/User/Downloads/vectorizer-66130701701.sav', 'rb'))
+
+# Set title
+st.title("Review Sentiment Prediction using Naive Bayes")
+
+# Text input for user input
+user_input = st.text_input("Enter your review:")
+
+# Transform user input using TfidfVectorizer
+user_input_vec = vectorizer.transform([user_input])
+
+# Predict sentiment
+pred = model_bay.predict(user_input_vec)
+
+# Display prediction result
+st.write("## Prediction Result:")
+st.write('Sentiment:', pred[0])
